f84119afe047f850273e1cec3d62ccebaefde085
guer <332724213@qq.com>
2017-12-04 17:15:56 +0800

    优化界面显示

--- src/views/Shop/ShopList.vue
+++ src/views/Shop/ShopList.vue
@@ -8,106 +8,101 @@
         <el-row class="search-row">
             <el-form class="inline-form" :inline="true">
                 <el-form-item label="搜索店铺">
-                    <el-input placeholder="请输入店铺名称" icon="search" v-model="searchContent" :on-icon-click="searchShop">
-                    </el-input>
+                    <el-input placeholder="请输入店铺名称" v-model="params.shopNameLike" />
                 </el-form-item>
                 <el-form-item label="店铺审核状态">
-                    <el-select v-model="audit" placeholder="请选择店铺审核状态" @change="searchShop">
-                        <el-option
-                          v-for="(item,index) in shopAuditStatusList"
-                          :key="index"
-                          :label="item.key"
-                          :value="item.value">
-                        </el-option>
+                    <el-select v-model="params.audit" placeholder="请选择店铺审核状态">
+                        <el-option v-for="(item,index) in shopAuditStatusList" :key="index" :label="item.key" :value="item.value" />
                     </el-select>
                 </el-form-item>
                 <el-form-item label="配送审核状态">
-                    <el-select v-model="deliveryAuditStatus" placeholder="请选择配送审核状态" @change="searchShop">
-                        <el-option
-                          v-for="(item,index) in deliveryAuditStatusList"
-                          :key="index"
-                          :label="item.key"
-                          :value="item.value">
-                        </el-option>
+                    <el-select v-model="params.deliveryAuditStatus" placeholder="请选择配送审核状态">
+                        <el-option v-for="(item,index) in deliveryAuditStatusList" :key="index" :label="item.key" :value="item.value" />
                     </el-select>
                 </el-form-item>
                 <el-form-item label="店铺状态">
-                    <el-select v-model="shelves" placeholder="请选择店铺状态" @change="searchShop">
-                        <el-option
-                            label="全部"
-                            value=" ">
-                        </el-option>
-                        <el-option
-                            label="上架"
-                            :value="true">
-                        </el-option>
-                        <el-option
-                            label="下架"
-                            :value="false">
-                        </el-option>
+                    <el-select v-model="params.shelves" placeholder="请选择店铺状态">
+                        <el-option label="全部" value=" " />
+                        <el-option label="上架" :value="true" />
+                        <el-option label="下架" :value="false" />
                     </el-select>
                 </el-form-item>
                 <el-form-item label="营业状态">
-                    <el-select v-model="operatingState" placeholder="请选择营业状态" @change="searchShop">
-                        <el-option
-                            label="全部"
-                            value=" ">
-                        </el-option>
-                        <el-option
-                            label="营业中"
-                            :value="true">
-                        </el-option>
-                        <el-option
-                            label="歇业中"
-                            :value="false">
-                        </el-option>
+                    <el-select v-model="params.operatingState" placeholder="请选择营业状态">
+                        <el-option label="全部" value=" " />
+                        <el-option label="营业中" :value="true" />
+                        <el-option label="歇业中" :value="false" />
                     </el-select>
                 </el-form-item>
+                <br>
+		        <el-form-item label="地区">
+		            <el-select v-model.number="params.provinceId" placeholder="不限" @change="provinceChange">
+					    <el-option v-for="(item,index) in provinceList" :key="index" :label="item.provinceName" :value="item.provinceId" />
+					</el-select>
+		        </el-form-item>
+		        <el-form-item label="">
+		            <el-select v-model.number="params.cityId" placeholder="不限" @change="cityChange">
+					    <el-option v-for="(item,index) in cityList" :key="index" :label="item.cityName" :value="item.cityId" />
+					</el-select>
+		        </el-form-item>
+		        <el-form-item label="">
+		            <el-select v-model.number="params.areaId" placeholder="不限" @change="areaChange">
+					    <el-option v-for="(item,index) in areaList" :key="index" :label="item.areaName" :value="item.areaId" />
+					</el-select>
+		        </el-form-item>
+		        <el-form-item label="">
+			        <el-button type="primary" @click="searchShop">搜索</el-button>
+		        </el-form-item>
             </el-form>
         </el-row>
         <el-row>
             <el-col>
-                <el-table :data="shopList">
-                    <el-table-column label="名称" width="240px" align="center">
-                        <template slot-scope="scope">{{scope.row.shopName?scope.row.shopName:'-'}}</template>
+                <el-table :data="shopList" :row-class-name="tableRowClassName">
+                    <el-table-column label="名称" align="center">
+                        <template slot-scope="scope">{{scope.row.shopName ? scope.row.shopName : '-'}}</template>
                     </el-table-column>
-                    <el-table-column prop="name" label="负责人" align="center"></el-table-column>
-                    <!-- <el-table-column label="LOGO" align="center" width="140px">
-                        <template slot-scope="scope"><img :src="UPLOADURL + scope.row.logoUrl + '/shopLogo.png'" alt="" class="logo-img"></template>
-                    </el-table-column> -->
-                    <el-table-column label="地址" width="280px" align="center">
-                        <template slot-scope="scope">{{scope.row.address?scope.row.address:'-'}}</template>
-                    </el-table-column>
-                    <el-table-column label="营业时间" width="130px" align="center">
-                        <template slot-scope="scope">{{scope.row.busBeginTime?scope.row.busBeginTime:'00:00'}} - {{scope.row.busEndTime?scope.row.busEndTime:'00:00'}}</template>
-                    </el-table-column>
-                    <!-- <el-table-column label="注册账号" width="160px" align="center">
-                        <template slot-scope="scope">{{scope.row.phoneNum?scope.row.phoneNum:'-'}}</template>
-                    </el-table-column> -->
-                    <el-table-column label="联系电话" width="140px" align="center">
-                        <template slot-scope="scope">{{scope.row.takeOutPhone?scope.row.takeOutPhone:'-'}}</template>
-                    </el-table-column>
-                    <el-table-column label="营业状态" align="center">
-                        <template slot-scope="scope">{{scope.row.operatingState?'营业中':'歇业中'}}</template>
-                    </el-table-column><el-table-column label="状态" align="center">
-                        <template slot-scope="scope">{{scope.row.shelves?'上架':'下架'}}</template>
-                    </el-table-column>
-                    <el-table-column label="配送状态" align="center">
-                        <template slot-scope="scope">{{formatAuditStatus(scope.row.deliveryAuditStatus)}}</template>
-                    </el-table-column>
-                    <el-table-column label="结算模板" align="center">
-                        <template scope="scope">{{scope.row.settlementTemplateName}}</template>
-                    </el-table-column>
-                    <el-table-column label="置顶值" align="center">
-                        <template slot-scope="scope">{{scope.row.topper}}</template>
-                    </el-table-column>
-                        <el-table-column label="操作" width="200px" align="center">
+                    <el-table-column label="负责人" width="160px" align="center">
                         <template slot-scope="scope">
-                            <el-button size="small" type="primary" @click="putAwayShop(scope.$index, scope.row)" v-if="!scope.row.shelves">上架</el-button>
-                            <el-button size="small" type="danger" @click="soldOutShop(scope.$index, scope.row)" v-else>下架</el-button>
-                            <el-button size="small" @click="openToperPopup(scope.$index, scope.row)">置顶</el-button>
+                        	{{scope.row.name}}<br>
+                        	({{scope.row.phoneNum}})
+                        </template>
+                    </el-table-column> 
+                    <!-- 
+                    <el-table-column label="LOGO" align="center" width="140px">
+                        <template slot-scope="scope"><img :src="UPLOADURL + scope.row.logoUrl + '/shopLogo.png'" alt="" class="logo-img"></template>
+                    </el-table-column> 
+                    -->
+                    <el-table-column label="地址" align="center" >
+                        <template slot-scope="scope">
+                        	{{scope.row.provinceName}} {{scope.row.cityName}} {{scope.row.areaName}}<br>
+                        	{{scope.row.address ? scope.row.address : '-'}}
+                        </template>
+                    </el-table-column>
+                    <el-table-column label="营业时间" width="140px" align="center">
+                        <template slot-scope="scope">{{scope.row.busBeginTime ? scope.row.busBeginTime : '00:00'}} - {{scope.row.busEndTime ? scope.row.busEndTime : '00:00'}}</template>
+                    </el-table-column>
+                    <el-table-column label="联系电话" width="140px" align="center">
+                        <template slot-scope="scope">{{scope.row.takeOutPhone ? scope.row.takeOutPhone : '-'}}</template>
+                    </el-table-column>
+                    <el-table-column label="状态" width="300px" align="center">
+                        <template slot-scope="scope">
+                        	<span class="operatingState">{{formatOperatingState(scope.row.operatingState)}}</span>/<span class="shelves">{{formatShelves(scope.row.shelves)}}</span>/<span class="deliveryAuditStatus">{{formatAuditStatus(scope.row.deliveryAuditStatus)}}</span>
+                        </template>
+                    </el-table-column>
+                    <el-table-column label="结算模板" width="140px" align="center">
+                        <template slot-scope="scope">{{scope.row.settlementTemplateName}}</template>
+                    </el-table-column>
+                    <el-table-column prop="topper" label="置顶值" width="100px" align="center" />
+                    <el-table-column label="操作" width="400px" align="center">
+                        <template slot-scope="scope">
+                            <el-button size="mini" type="primary" @click="putAwayShop(scope.$index, scope.row)" v-if="!scope.row.shelves">上架</el-button>
+                            <el-button size="mini" type="danger" @click="soldOutShop(scope.$index, scope.row)" v-else>下架</el-button>
+                            <el-button size="mini" @click="openToperPopup(scope.$index, scope.row)">置顶</el-button>
                             <router-link :to="'/shopDetail?shopId='+scope.row.shopId" class="link">
-                                <el-button size="small" type="primary">详情</el-button>
+                                <el-button size="mini" type="primary">详情</el-button>
+                            </router-link>
+                            <router-link :to="'/shopPrint?shopId='+scope.row.shopId" class="link">
+                                <el-button size="mini">打印机列表</el-button>
                             </router-link>
                         </template>
                     </el-table-column>
@@ -128,7 +123,7 @@
         <el-row>
             <!-- 分页 -->
             <el-col class="pagination">
-                <el-pagination @current-change="currentChange" :current-page="pageId" :page-size="pageSize" layout="total, prev, pager, next" :total="counts">
+                <el-pagination @current-change="currentChange" :current-page="params.pageId" :page-size="params.pageSize" layout="total, prev, pager, next" :total="counts">
                 </el-pagination>
             </el-col>
         </el-row>
@@ -137,6 +132,9 @@
 </template>
 <script>
 import {
+	getProvinceList,
+	getCityList,
+	getAreaList,
     shopList,
     putAway,
     soldOut,
@@ -146,21 +144,30 @@ import {
 export default {
     data: function() {
         return {
+        	params: {
+        		shopNameLike: null,
+        		audit: null,
+            	deliveryAuditStatus: null,
+            	shelves: null,
+           		operatingState: null,
+           		provinceId: null,
+           		cityId: null,
+           		areaId: null,
+	            pageId: 1,
+	            pageSize: 20,
+        	},
+            shopAuditLists: null,
+            provinceList: [],
+			cityList: [],
+			areaList: [],
+			cityListCache: {},
+			areaListCache: {},
             addLoading: false,
             addToperPopup: false,
-            searchContent: '',
-            pageId: 1,
-            pageSize: 20,
             counts: 0,
             isAdd: true,
-            shopList: null,
+            shopList: [],
             shopId: 0,
-            audit: '',
-            deliveryAuditStatus: '',
-            audit: '',
-            shelves: '',
-            shopType: '',
-            operatingState: '',
             formInline: {
                 topper: 0
             },
@@ -199,14 +206,57 @@ export default {
         }
     },
     created: function() {
+		getProvinceList().then(data => {
+			this.provinceList = [{
+				provinceName: '不限',
+				provinceId: null
+			}]
+			this.provinceList = this.provinceList.concat(data)
+		})
         this.pageId = parseInt(this.$route.query.page) || 1;
         this.getShopLists();
     },
     methods: {
+		provinceChange: function(value) {
+			this.params.provinceId = value;
+			if (this.cityListCache[value]) {
+				this.cityList = this.cityListCache[value];
+			} else {
+				getCityList(value).then(data => {
+					var temp = [{
+						cityName: '不限',
+						cityId: null
+					}]
+					data = temp.concat(data)
+				
+					this.cityListCache[value] = data;
+					this.cityList = data;
+				})
+			}
+		},
+		cityChange: function(value) {
+			this.params.cityId = value;
+			if (this.areaListCache[value]) {
+				this.areaList = this.areaListCache[value];
+			} else {
+				getAreaList(value).then(data => {
+					var temp = [{
+						areaName: '不限',
+						areaId: null
+					}]
+					data = temp.concat(data)
+					
+					this.areaListCache[value] = data;
+					this.areaList = data;
+				})
+			}
+		},
+		areaChange: function(value) {
+			this.params.areaId = value;
+		},
         //获取列表
         getShopLists: function() {
-            shopList({ params: { pageId: this.pageId, pageSize: this.pageSize, shopNameLike: this.searchContent,deliveryAuditStatus: this.deliveryAuditStatus, audit: this.audit, shelves: this.shelves, operatingState: this.operatingState } }).then(data => {
-                console.log(data)
+            shopList({ params: this.params }).then(data => {
                 this.counts = data.count;
                 this.shopList = data.list;
             })
@@ -293,9 +343,15 @@ export default {
                 this.cancelSetToper()
             })
         },
+        formatOperatingState: function(value){
+        	return value ? '营业中' : '歇业中';
+        },
+        formatShelves: function(value){
+        	return value ? '上架' : '下架';
+        },
         formatAuditStatus: function(auditType){
             switch(auditType){
-                case 'UN_COMMIT,':
+                case 'UN_COMMIT':
                     return '未提交';
                 case 'UN_AUDIT':
                     return '未审核';
@@ -306,12 +362,67 @@ export default {
                 case 'UNADOPT':
                     return '审核不通过';
             }
+        },
+        tableRowClassName: function(row, rowIndex) {
+        	var classNames = "";
+        	
+        	if (!row.operatingState) {
+        		classNames += "operatingState_Off ";
+        	}
+        	
+        	if (!row.shelves) {
+        		classNames += "shelves_Off ";
+        	}
+        	
+        	if (row.deliveryAuditStatus != 'ADOPT') {
+        		classNames += "deliveryAuditStatus_" + row.deliveryAuditStatus + " ";
+        	}
+        	
+        	if (classNames != '') {
+        		classNames += "warning-row ";
+        	}
+        	
+        	return classNames;
         }
     }
 }
 </script>
 <style scoped lang="scss">
 .lives-news {
+	.operatingState_Off{
+		.operatingState {
+    		color: gray;
+		}
+	}
+	.shelves_Off{
+		.shelves {
+    		color: red;
+		}
+	}
+	
+	.deliveryAuditStatus_UN_COMMIT {
+		.deliveryAuditStatus {
+    		color: gray;
+		}
+	}
+	.deliveryAuditStatus_UN_AUDIT {
+		.deliveryAuditStatus {
+    		color: #929227;
+		}
+	}
+	.deliveryAuditStatus_IN_THE_REVIEW {
+		.deliveryAuditStatus {
+    		color: green;
+		}
+	}
+	.deliveryAuditStatus_UNADOPT {
+		.deliveryAuditStatus {
+    		color: red;
+		}
+	}
+	.warning-row {
+    	background: oldlace;
+  	}
     .title {
         font-size: 12px;
         border-bottom: 1px solid #eaeaea;
@@ -327,6 +438,9 @@ export default {
     }
     .logo-img{
         width: 120px;
+    }
+    .gray {
+    	color: gray;
     }
     .link{
         display: inline-block;
