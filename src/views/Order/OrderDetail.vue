9467a19022e7be291424d09734d313b2cbfd4b0c
xuejiangjun <449590828@qq.com>
2017-12-14 11:52:37 +0800

    修改订单详情

--- src/views/Order/OrderDetail.vue
+++ src/views/Order/OrderDetail.vue
@@ -12,44 +12,128 @@
         </el-col>
         <el-col>
             <ul class="order-lists" v-if="orderLists">
-                <li>订单id：{{orderLists.orderId}}</li>
+                <li>店铺ID：{{orderLists.shopId}}</li>
+                <li>店铺名称：{{orderLists.shopName}}</li>
+                <li>店铺电话：{{orderLists.phoneNum}}</li>
+                <li>订单ID：{{orderLists.orderId}}</li>
                 <li>订单号：{{orderLists.orderNum}}</li>
                 <li>订单名称：{{orderLists.orderName}}</li>
                 <li>订单价格：{{orderLists.orderPrice?formatMoney(orderLists.orderPrice):'0.00'}}元</li>
+                <li>订单原价：{{orderLists.originalPrice?formatMoney(orderLists.originalPrice):'0.00'}}元</li>
+                <li>订单商品数量：{{orderLists.orderGoodsCount}}</li>
+                <li>订单商品总价：{{formatMoney(orderLists.orderGoodsPrice) + '元'}}</li>
+                <li>订单原商品总价：{{formatMoney(orderLists.originalGoodsPrice) + '元'}}</li>
+                <li>订单优惠价格：{{orderLists.favourablePrice}}元</li>
                 <li>订单状态：{{formatOrderStatus(orderLists.orderStatus)}}</li>
-                <li v-if="orderLists.orderStatus=='CANCELLATION'">取消类型：{{formatOrderCancelType(orderLists.orderCancel.cancelType)}}</li>
-                <li v-if="orderLists.orderStatus=='CANCELLATION'">取消原因：{{formatOrderCancelReason(orderLists.orderCancel.orderCancelReason)}}</li>
-                <li>订单类型：{{formatOrderType(orderLists.orderType)}}</li>
-                <!-- <li>订单支付SN号：{{orderLists.paymentSN}}</li> -->
+                <li v-if="orderLists.orderStatus=='CANCELLATION'">
+                    <div>订单取消：
+                        <p class="driver">取消原因：{{formatOrderCancelReason(orderLists.orderCancel.orderCancelReason)}}<br>
+                        取消类型：{{formatOrderCancelType(orderLists.orderCancel.cancelType)}}<br>
+                        原因描述：{{orderLists.orderCancel.cancelContent}}</p>
+                    </div>
+                </li>
                 <li>订单备注：{{orderLists.orderContent?orderLists.orderContent:'-'}}</li>
-                <li>订单添加时间：{{moment(orderLists.addTime).format('YYYY-MM-DD HH:mm:ss')}}</li>
-                <li>联系人：{{orderLists.orderContact.contactName?orderLists.orderContact.contactName:'-'}}</li>
-                <li>联系电话：{{orderLists.orderContact.contactPhone?orderLists.orderContact.contactPhone:'-'}}</li>
-                <li>地址：{{orderLists.orderContact.address?orderLists.orderContact.address:'-'}}</li>
-                <!-- <li>优惠券详情：{{orderLists.coupon}}</li> -->
-                <!-- <li>可使用优惠券数量：{{orderLists.couponNum}}</li> -->
-                <li>优惠券序列号：{{orderLists.couponSNId}}</li>
+                <li v-if="orderLists.receivingTimeoutTime">接单超时时间：{{moment(orderLists.receivingTimeoutTime).format('YYYY-MM-DD HH:mm:ss')}}</li>
+                <li>订单类型：{{formatOrderType(orderLists.orderType)}}</li>
+                <li v-if="orderLists.orderType == 'RESERVE'">
+                    <div>预定订单信息：
+                        <p class="driver">是否包房：{{orderLists.orderReserve.isPrivateRoom?'是':'否'}}<br>就餐人数：{{orderLists.orderReserve.repastNum}}<br>
+                        就餐时间：{{orderLists.orderReserve.repastTime}}</p>
+                    </div>
+                </li>
+                <li v-else>
+                    <div>外卖订单信息：
+                        <p class="driver">配送员姓名：{{orderLists.orderTakeout.carrierDriverName || '-'}}<br>
+                        联系电话：{{orderLists.orderTakeout.carrierDriverphone || '-'}}<br>
+                        配送时间：{{moment(orderLists.orderTakeout.deliveryTime).format('YYYY-MM-DD HH:mm:ss')}}<br>
+                        配送方式：{{formatDeliveryType(orderLists.orderTakeout.distributionType)}}<br>
+                        餐盒费：{{formatMoney(orderLists.orderTakeout.mealFee)}}元<br>
+                        配送费：{{formatMoney(orderLists.orderTakeout.shippingFee)}}元</p>
+                    </div>
+                </li>
+                <li v-if="orderLists.orderContact">
+                    <div>联系人信息：
+                        <p class="driver">联系人ID：{{orderLists.orderContact.contactId}}<br>联系人：{{orderLists.orderContact.contactName?orderLists.orderContact.contactName:'-'}}<br>
+                        性别：{{formatGender(orderLists.orderContact.gender)}}<br>
+                        联系电话：{{orderLists.orderContact.contactPhone?orderLists.orderContact.contactPhone:'-'}}<br>
+                        地址：{{orderLists.orderContact.address?orderLists.orderContact.address:'-'}}<br>
+                        简单地址：{{orderLists.orderContact.simpleAddress?orderLists.orderContact.simpleAddress:'-'}}<br>
+                        详细地址：{{orderLists.orderContact.detailAddress?orderLists.orderContact.detailAddress:'-'}}<br>
+                        经度：{{orderLists.orderContact.longitude?orderLists.orderContact.longitude:'-'}}<br>
+                        纬度：{{orderLists.orderContact.latitude?orderLists.orderContact.latitude:'-'}}<br>
+                        门牌号：{{orderLists.orderContact.houseNumber?orderLists.orderContact.houseNumber:'-'}}<br>
+                    </p>
+                    </div>
+                </li>
+                <li v-if="orderLists.coupon">
+                    <div>优惠券信息
+                        <p class="driver">优惠券ID：{{orderLists.coupon.couponId}}<br>
+                        优惠券名称：{{orderLists.coupon.couponName}}<br>
+                        优惠券金额：{{orderLists.coupon.money}}<br>
+                        </p>
+                    </div>
+                </li>
+                <li>优惠券序列号：{{orderLists.couponSNId || '-'}}</li>
                 <li>是否支付完成：{{orderLists.isFinishPay?'是':'否'}}</li>
-                <li>订单支付时间：{{orderLists.payTime?moment(orderLists.payTime).format('YYYY-MM-DD HH:mm:ss'):'-'}}</li>
-                <li>订单完成时间：{{orderLists.finishTime?moment(orderLists.finishTime).format('YYYY-MM-DD HH:mm:ss'):'-'}}</li>
+                <li>是否退款：{{orderLists.isRefund?'是':'否'}}</li>
                 <li>支付方式：{{formatPayType(orderLists.payment)}}</li>
-                <li v-if="orderLists.orderTakeout.carrierDriverName">
+                <li>订单支付SN号：{{orderLists.paymentSN}}</li>
+                <li>订单添加时间：{{moment(orderLists.addTime).format('YYYY-MM-DD HH:mm:ss')}}</li>
+                <li>订单支付时间：{{orderLists.payTime?moment(orderLists.payTime).format('YYYY-MM-DD HH:mm:ss'):'-'}}</li>
+                <li>订单送达时间：{{moment(orderLists.deliveredTime).format('YYYY-MM-DD HH:mm:ss')}}</li>
+                <li>订单完成时间：{{orderLists.finishTime?moment(orderLists.finishTime).format('YYYY-MM-DD HH:mm:ss'):'-'}}</li>
+                <li>订单完成超时时间：{{orderLists.finishTimeoutTime?moment(orderLists.finishTimeoutTime).format('YYYY-MM-DD HH:mm:ss'):'-'}}</li>
+                <!-- <li v-if="orderLists.orderTakeout.carrierDriverName">
                     <div>骑手信息：
                         <p class="driver">骑手名字：{{orderLists.orderTakeout.carrierDriverName}}</p>
                         <p class="driver">联系电话：{{orderLists.orderTakeout.carrierDriverphone}}</p>
                     </div>
+                </li> -->
+                <li v-if="orderLists.orderInvoiceInfo">
+                    <div>订单发票信息：
+                        <p class="driver">抬头：{{orderLists.orderInvoiceInfo.title || '-'}}<br>
+                        发票类型：{{formatInvoiceType(orderLists.orderInvoiceInfo.invoiceType)}}<br>
+                        税号：{{orderLists.orderInvoiceInfo.ein || '-'}}<br>
+                        </p>
+                    </div>
                 </li>
-                <li v-if="orderLists.orderType == 'RESERVE'">就餐人数：{{orderLists.repastNum}}人</li>
-                <li v-if="orderLists.orderType == 'RESERVE'">就餐时间：{{moment(orderLists.repastTime).format('YYYY-MM-DD HH:mm:ss')}}</li>
-                <li>是否包房：{{orderLists.isPrivateRoom?'是':'否'}}</li>
+                <li>订单活动：
+                    <el-table :data="orderLists.orderActivitys" style="width: 800px; margin-left: 20px;">
+                        <el-table-column label="序号" type="index" width="100" align="center"></el-table-column>
+                        <el-table-column property="activityId" label="活动ID" align="center" width="80px"></el-table-column>
+                        <el-table-column property="activityReduced" label="优惠金额" align="center" width="100px"></el-table-column>
+                        <el-table-column label="活动类型" align="center">
+                            <template slot-scope="scope">
+                                {{formatActivityType(scope.row.activityType)}}
+                            </template>
+                        </el-table-column>
+                        <el-table-column property="activityContent" label="内容描述" align="center"></el-table-column>
+                    </el-table>
+                </li>
                 <li v-if="orderLists.orderGoods.length">订单商品：
-                    <el-table :data="orderLists.orderGoods" style="width: 800px; margin-left: 20px;">
+                    <el-table :data="orderLists.orderGoods" style="width: 1200px; margin-left: 20px;">
                         <el-table-column label="序号" type="index" width="100" align="center"></el-table-column>
                         <el-table-column property="goodsName" label="商品名称" align="center"></el-table-column>
-                        <el-table-column property="goodsCount" label="商品数量" align="center"></el-table-column>
-                        <el-table-column property="goodsPrice" label="商品价格" align="center">
+                        <el-table-column property="goodsId" label="商品ID" align="center" width="100px"></el-table-column>
+                        <el-table-column property="goodsCount" label="数量" align="center" width="80px"></el-table-column>
+                        <el-table-column label="商品价格" align="center">
                             <template slot-scope="scope">
                                 {{scope.row.goodsPrice?formatMoney(scope.row.goodsPrice):'0.00'}}元
+                            </template>
+                        </el-table-column>
+                        <el-table-column label="原价小计" align="center">
+                            <template slot-scope="scope">
+                                {{scope.row.oldAmount?formatMoney(scope.row.oldAmount):'0.00'}}元
+                            </template>
+                        </el-table-column>
+                        <el-table-column label="现价小计" align="center">
+                            <template slot-scope="scope">
+                                {{scope.row.amount?formatMoney(scope.row.amount):'0.00'}}元
+                            </template>
+                        </el-table-column>
+                        <el-table-column label="添加时间" align="center">
+                            <template slot-scope="scope">
+                                {{moment(scope.row.addTime).format('YYYY-MM-DD HH:mm:ss')}}
                             </template>
                         </el-table-column>
                     </el-table>
@@ -165,6 +249,54 @@ export default {
                 default:
                     return '-'
             }
+        },
+        formatDeliveryType: function(type){
+            switch(type){
+                case 'SELF_DELIVERY_BY_MERCHANTS':
+                    return '商家自送';
+                case 'ANUBIS':
+                    return '蜂鸟';
+                default:
+                    return '-'
+            }
+        },
+        formatGender: function(gender){
+            switch(gender){
+                case 'MALE':
+                    return '先生';
+                case 'FEMALE':
+                    return '女士';
+                case 'UNKNOW':
+                    return '未知';
+                default:
+                    return '-'
+            }
+        },
+        formatActivityType: function(type){
+            switch(type){
+                case 'FIRST':
+                    return '首单立减';
+                case 'DELGOLD':
+                    return '购满就减';
+                case 'COMPLIMENTARY':
+                    return '购满就送';
+                case 'SPECIALPRICES':
+                    return '特价商品';
+                case 'SALE':
+                    return '折扣商品';
+                case 'SPECIFIC':
+                    return '其他';
+            }
+        },
+        formatInvoiceType: function(type){
+             switch(type){
+                case 'COMPANY':
+                    return '公司';
+                case 'INDIVIDUAL':
+                    return '个人';
+                default:
+                    return '-'
+            }
         }
     }
 }
@@ -191,7 +323,7 @@ export default {
         font-size: 16px;
     }
     .driver{
-        padding-left: 20px;
+        padding-left: 40px;
         margin: 0;
     }
 }
